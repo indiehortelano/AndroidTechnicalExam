bbb73d9a661fae196405f05ca0fe08c092c83095
ihortelano <i.hortelano@arcanys.com>
2015-08-03 19:22:05 +0800

    adding retrofit again

---
+++ app/src/main/java/com/androidtechnicalexam/app/retrofit/Retrofit.java
@@ -0,0 +1,32 @@
+package com.androidtechnicalexam.app.retrofit;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.bind.DateTypeAdapter;
+
+import java.util.Date;
+
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+
+/**
+ * Created by ihortelano on 8/3/15.
+ */
+public class Retrofit {
+
+    public static <K> K getService(Class<K> claz) {
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .registerTypeAdapter(Date.class, new DateTypeAdapter())
+                .create();
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://jsonplaceholder.typicode.com")
+                .setConverter(new GsonConverter(gson))
+                .build();
+
+        return restAdapter.create(claz);
+    }
+
+}
