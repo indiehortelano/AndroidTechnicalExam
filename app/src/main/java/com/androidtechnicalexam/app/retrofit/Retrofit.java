03e8516da5613b02bf712c67b5cd88eca2a71f01
ihortelano <i.hortelano@arcanys.com>
2015-08-03 20:45:46 +0800

    added roboguice for dependency injection

--- app/src/main/java/com/androidtechnicalexam/app/retrofit/Retrofit.java
+++ app/src/main/java/com/androidtechnicalexam/app/retrofit/Retrofit.java
@@ -4,6 +4,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.internal.bind.DateTypeAdapter;
+import com.google.inject.Singleton;
 
 import java.util.Date;
 
@@ -13,6 +14,7 @@ import retrofit.converter.GsonConverter;
 /**
  * Created by ihortelano on 8/3/15.
  */
+@Singleton
 public class Retrofit {
 
     public static <K> K getService(Class<K> claz) {
